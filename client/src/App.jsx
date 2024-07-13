6ed439d7841710f33c20c41a2a1721d59dd6a939
nuran912 <nuranalwis@gmail.com>
2024-07-13 13:04:25 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-//type rfc and press enter to get initial App structure
+//type rfc and press enter to create an initial react functional component
 
-import React from 'react'
+import { BrowserRouter, Routes, Route }  from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Projects from './Pages/Projects'
+import Dashboard from './Pages/Dashboard'
 
 export default function App() {
   return (
-    <h1>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} /> 
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
