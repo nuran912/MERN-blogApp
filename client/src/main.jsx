906232f271838081d9ade16455e4e8d223b69262
nuran912 <nuranalwis@gmail.com>
2024-08-08 03:57:55 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 
 //below 2 imports are used for the redux toolkit
-import { store } from './redux/store.js'
+import { store, persistor } from './redux/store.js'
 import { Provider } from 'react-redux'  //Provider is necessary to use store
+import { PersistGate } from 'redux-persist/integration/react' //this is used to cover everything
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>  
+      <App />
+    </Provider>
+  </PersistGate>
+);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice.js'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({ //instead of having a lot of reducers, we can use a root reducer as a combination of all the reducers
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root', //key is the name bing stored
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  }
-})
+  reducer: persistedReducer,  
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware( //if we don't use the middleware, we'll get an error
+    {serializableCheck: false}
+  ),
+});
+
+export const persistor = persistStore(store);
