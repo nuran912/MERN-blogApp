bae32646073b7499d5836fe23a421b4434164905
nuran912 <nuranalwis@gmail.com>
2024-08-09 21:53:33 +0530

    complete sidebar of the dashboard

---
+++ client/src/Components/DashSidebar.jsx
@@ -0,0 +1,37 @@
+import { Sidebar } from 'flowbite-react'
+import { useEffect, useState } from 'react';
+import { HiUser, HiArrowSmRight } from 'react-icons/hi'
+import { Link, useLocation } from 'react-router-dom';
+
+export default function DashSidebar() {
+
+    const location = useLocation(); //initialize useLocation
+    const [tab, setTab] = useState('');
+    
+    //evach time we come to this page, we get irs tab⬇️
+    useEffect( ()=> {
+      const urlParams = new URLSearchParams(location.search); //URLSearchParams returns parameters
+      const tabFromUrl = urlParams.get('tab');
+      if(tabFromUrl){ //if the tab from the url is not null, do⬇️
+        setTab(tabFromUrl)
+      }
+    }, [location.search]) //this useEffect renders every time location.search updates  
+
+  return (
+    <Sidebar className='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>  {/* when profile is clicked, navigate to profile tab */}
+                {/* active only when in the profile tab ⬇️ */}
+                <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                    Profile
+                </Sidebar.Item>
+                </Link>
+                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                    Sign Out
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import DashSidebar from '../Components/DashSidebar';
+import DashProfile from '../Components/DashProfile';
 
 export default function Dashboard() {
+
+  const location = useLocation(); //initialize useLocation
+  const [tab, setTab] = useState('');
+  
+  //evach time we come to this page, we get irs tab⬇️
+  useEffect( ()=> {
+    const urlParams = new URLSearchParams(location.search); //URLSearchParams returns parameters
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){ //if the tab from the url is not null, do⬇️
+      setTab(tabFromUrl)
+    }
+  }, [location.search]) //this useEffect renders every time location.search updates
+
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar/>
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile/>} {/* DashProfile is only visible when we're in the profile tab */}
+    </div>
   )
 }
